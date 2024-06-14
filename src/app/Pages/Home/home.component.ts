6e029e29d7505272be828a5f4dea6af37003fe22
Davi Komura de Castro <88511573+davikomura@users.noreply.github.com>
2024-06-14 14:54:55 +0000

    Atualização

--- src/app/Pages/Home/home.component.ts
+++ src/app/Pages/Home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  newsList = [
+    { title: 'Título da Notícia 1' },
+    { title: 'Título da Notícia 2' },
+    { title: 'Título da Notícia 3' },
+    { title: 'Título da Notícia 4' },
+    { title: 'Título da Notícia 5' }
+  ];
 
 }
